1346b1a63d3335ba5152b2518bad3ce2e68255b4
Andrew Tenajeros <jules.tenajeros@gmail.com>
2018-09-19 00:36:48 +0800

    add url to image

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -28,7 +28,7 @@
     <div class="row">
             <div class="col s12 m6">
             </br>
-                <img class="responsive-image" src="/storage/cover_images/{{$post->cover_image}}"> </br>
+                <img class="responsive-image" src="URL::to('/storage/cover_images/{{$post->cover_image}}')"> </br>
             </div>
         <br><br>
         <div class="col s12 m6">
