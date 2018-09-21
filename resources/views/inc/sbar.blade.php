95eba9dc40042bcf8276a5d7367cec5d2ac7b890
Andrew Tenajeros <jules.tenajeros@gmail.com>
2018-09-22 03:30:42 +0800

    add category search books

--- resources/views/inc/sbar.blade.php
+++ resources/views/inc/sbar.blade.php
@@ -19,9 +19,10 @@
 
     <ul class="main_menu">
             <li class="p-b-9">
-            <a href="#" class="s-text7">
+            <!-- <button type="link" {{$search_category = "Books and references";}} value="{{ isset($search_category) ? $search_category : '' }}" class="s-text7"> -->
+            <button type="link" value="{{ isset(Books and references) ? Books and references : '' }}" class="s-text7">
                 Books and references
-            </a>
+            </button>
             </li>
            <li class="p-b-9">
             <a href="#" class="s-text7">
