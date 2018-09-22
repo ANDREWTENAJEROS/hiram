141820863b71b8481a2c1e024babecba1837468d
Andrew Tenajeros <jules.tenajeros@gmail.com>
2018-09-23 03:23:29 +0800

    add fb plugins

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,7 +23,7 @@
 	<link rel="stylesheet" type="text/css" href="../../../css/util.css">
     <link rel="stylesheet" type="text/css" href="../../../css/main.css">
 
-    <div id="fb-root"></div>
+    <div id="{{$post->id}}"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -38,7 +38,7 @@
             </br>
             <!-- <img src="{{ URL::to('/') }}/storage/cover_images/{{$post->cover_image}}" alt="{{$post->title}}" /> -->
             <!-- <img src="/storage/cover_images/{{$post->cover_image}}" /> -->
-            <img src="https://vfveqg.bn.files.1drv.com/y4mOKO9kyGQPENwvxIK_rFd5Wy2STZ9A-3n-zG4Nh7MxNqCSXDXd74tkSwkSGcmi1fr5ThvLe5n4ecCx1x76q8FlFlGB889N2HfX2PbC2npII847MYovW0oKpAWY4t8Lo7ugy48vgLeKNd1TGslJ7lK2dhfhWa4acdCLOKXFCP_vWhrkM0Y2ooLnbrVfTjCbNkt-sEYEpJil9fWGE2B12EMIw?width=1250&height=830&cropmode=none" />
+            <img  style="    width: 250px;" src="https://vfveqg.bn.files.1drv.com/y4mOKO9kyGQPENwvxIK_rFd5Wy2STZ9A-3n-zG4Nh7MxNqCSXDXd74tkSwkSGcmi1fr5ThvLe5n4ecCx1x76q8FlFlGB889N2HfX2PbC2npII847MYovW0oKpAWY4t8Lo7ugy48vgLeKNd1TGslJ7lK2dhfhWa4acdCLOKXFCP_vWhrkM0Y2ooLnbrVfTjCbNkt-sEYEpJil9fWGE2B12EMIw?width=1250&height=830&cropmode=none" />
                 <!-- <img class="responsive-image" src="URL::to('/storage/cover_images/{{$post->cover_image}}')"> -->
                  </br>
             </div>
