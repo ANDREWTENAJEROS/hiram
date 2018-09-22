218c84b7cd94d66dc1d1642cb006a9148c6a6b9b
Andrew Tenajeros <jules.tenajeros@gmail.com>
2018-09-22 12:51:47 +0800

    temp image

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -29,7 +29,8 @@
             <div class="col s12 m6">
             </br>
             <!-- <img src="{{ URL::to('/') }}/storage/cover_images/{{$post->cover_image}}" alt="{{$post->title}}" /> -->
-            <img src="/storage/cover_images/{{$post->cover_image}}" />
+            <!-- <img src="/storage/cover_images/{{$post->cover_image}}" /> -->
+            <img src="https://vfveqg.bn.files.1drv.com/y4mOKO9kyGQPENwvxIK_rFd5Wy2STZ9A-3n-zG4Nh7MxNqCSXDXd74tkSwkSGcmi1fr5ThvLe5n4ecCx1x76q8FlFlGB889N2HfX2PbC2npII847MYovW0oKpAWY4t8Lo7ugy48vgLeKNd1TGslJ7lK2dhfhWa4acdCLOKXFCP_vWhrkM0Y2ooLnbrVfTjCbNkt-sEYEpJil9fWGE2B12EMIw?width=1250&height=830&cropmode=none" width="1250" height="830" />
                 <!-- <img class="responsive-image" src="URL::to('/storage/cover_images/{{$post->cover_image}}')"> -->
                  </br>
             </div>
