b0fa8327a257106e0398fa5a279138aeda6f2442
Andrew Tenajeros <jules.tenajeros@gmail.com>
2018-10-04 09:37:10 +0800

    add landing page design

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,7 +7,7 @@
 
 <div class="t-center p-l-15 p-r-15">
                 <div class="t-center s-text8 p-t-20">
-                Many people have had many personal items but thought of having found no use of them anymore and thus having no idea of what to do of it, they could just put it up for sale, dispose of it or give it to someone else, but deep within them, they have thoughts of using them again and don’t plan on relinquishing it, and so with this renting platform called Hiram, people can now have their personal belongings to be lent to someone else for a price, of course. This paper aims to determine whether or not a rental and lending platform is needed by students. With Hiram, people can rent items of their choosing, be it for academic usage or personal usage for a certain amount of time.                </div>
+                Many people have had many personal items but thought of having found no use of them anymore and thus having no idea of what to do of it, they could just put it up for sale, dispose of it or give it to someone else, but deep within them, they have thoughts of using them again and don’t plan on relinquishing it, and so with this renting platform called Hiram, people can now have their personal belongings to be lent to someone else for a price, of course. With Hiram, people can rent items of their choosing, be it for academic usage or personal usage for a certain amount of time.                </div>
             </div>
 
     <div class="t-center p-l-15 p-r-15">
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,18 +7,25 @@
             <h1>{{$title}}</h1>
             <p>Rental Platform for Students</p>
         </div>
+        </br>
+        <img style="width:50%;" src="images/hiram_landing.png">
     @else   
      <div class="jumbotron text-center">
             <h1>{{$title}}</h1>
             <p> Rental Platform for Students </p>
             </br>
+
         <div class="row">
-        <div class="col s12 m6"  > <a class="login100-form-btn" href="/login" >Login</a></div>
+        <div class="col s12 m6"  > <a  class="login100-form-btn" href="/login" >Login</a></div>
         <div class="col s12 m6"  ><a class="login100-form-btn" href="/register"> Register</a></div>
+        </br>
+
 <!--             
             <a class="login100-form-btn" href="/login"><div class="col s12 m3" href="/login"> Download for Android</div></a>
             <a class="login100-form-btn" href="/login"><div class="col s12 m3" href="/login">Download for IOS</div></a> -->
         </div>
         </div>
+        <img  style="width:100%; left:0; bottom:0;" src="images/hiram_landing.png">
+
     @endif
 @endsection
