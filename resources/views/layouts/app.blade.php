fd78544254dc2674656210ca782828d46dbaa8d6
Andrew Tenajeros <jules.tenajeros@gmail.com>
2018-10-08 03:42:41 +0800

    update meta data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>H I R A M </title>
-<meta name="Description" content="hiram a lending and renting platform for students">
-<meta name="Keywords" content="borrow lend borrower lender rentee renting items school lend items ">
+<title>H I R A M - hiram a lending and rental platform for students </title>
+<meta name="Description" content="Hiram, people can now have their personal belongings to be lent to someone else for a price, of course. With Hiram, people can rent items of their choosing, be it for academic usage or personal usage for a certain amount of time.
+">
+<meta name="Keywords" content="borrow lend rent borrower lender rentee renting items school lend items hiram mag pa hiram mang hiram mg hiram pahiram lend books rent books ">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
